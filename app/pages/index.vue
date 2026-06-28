6e0f0af645be7cebf5625cff200b82297c6f8f19
novalsmith <novalsmith69.com>
2026-06-28 21:55:34 +0700

    update text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,9 @@
 <!-- pages/index.vue -->
 <template>
   <div>
-    <h1>Selamat Datang di Umami Food</h1>
-    <p>Silakan pilih menu di bawah ini:</p>
+    <h1 class="text-2xl font-bold">Selamat Datang di Umami Food</h1>
     <!-- Without prefix -->
 <UButton color="primary" variant="solid">Lihat Artikel</UButton>
-
-    <ul>
-      <li><NuxtLink to="/articles">Lihat Semua Artikel</NuxtLink></li>
-      <li><NuxtLink to="/recipes">Lihat Semua Resep</NuxtLink></li>
-    </ul>
+ 
   </div>
 </template>
